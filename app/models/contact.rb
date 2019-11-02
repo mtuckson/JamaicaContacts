b8e44f5f91d37d8c8b2d8edf6d64e8120cdea821
Nathan Giordano <ngiordano@zola.com>
2019-11-02 11:45:02 -0400

    Make the Ruby version slightly more flexible and clean up some things in the Contact model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -7,17 +7,17 @@ class Contact < ApplicationRecord
 
   mount_uploader :avatar, AvatarUploader
 
+  def is_baptized?
+    baptism_status == true
+  end
+
   def full_name
-    alias_string = self.alias.present? ? "\"#{self.alias}\"" : nil
+    alias_string = self.alias.present? ? "“#{self.alias}”" : nil
     [bro_or_sis, first_name, alias_string, last_name].compact.join(' ')
   end
 
   def bro_or_sis
-    if gender == "Male"
-      return "Brother"
-
-    elsif gender == "Female"
-      return "Sister"
-    end
+    return nil unless is_baptized?
+    Male? ? 'Bro.' : 'Sis.'
   end
 end
