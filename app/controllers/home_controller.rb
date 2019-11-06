1a79f1628557f9e054ebc3e3c09bdc1b6ba4c4de
Matthew Tuckson <mtuck017@odu.edu>
2019-11-05 19:00:19 -0500

    Changed navbar to include search bar. Styled search results in the home page. Created partials for ecclesia lists and contact lists.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!
   def index
-    if params[:search]
+    if params[:search] && (params[:search].blank? == false)
       @ecclesia = Ecclesium.where("name LIKE ?","%#{params[:search]}%").to_json.html_safe
       @contacts = Contact.where("first_name LIKE ? or last_name LIKE ?", "%#{params[:search]}%", "%#{params[:search]}%").to_json.html_safe
       @ecclesiaNoJson = Ecclesium.where("name LIKE ?","%#{params[:search]}%")
--- app/models/ecclesium.rb
+++ app/models/ecclesium.rb
@@ -6,4 +6,11 @@ class Ecclesium < ApplicationRecord
   after_validation :geocode, if: :postal_address_changed?
 
   mount_uploader :avatar, AvatarUploader
+
+  def recording_brother
+    unless recording_brother_id.blank?
+  return  recording_brother = Contact.find(recording_brother_id)
+  end
+  return nil
+  end
 end
